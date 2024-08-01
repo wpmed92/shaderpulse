c3306d576cb9fffe2031f981b086257b7516ee69
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2024-08-01 17:43:40 +0200

    Fix struct parsing

--- example/passthrough_fragment.glsl
+++ example/passthrough_fragment.glsl
@@ -1,11 +1,15 @@
 layout(location = 0) out vec4 outColor;
 
-struct Test {
-  float a;
+// Demonstrate struct handling
+struct Color {
+  float r;
+  float g;
   float b;
+  float a;
 }
+
 void main() {
-  Test t = Test(1.0, 1.0);
-  outColor = vec4(t.a, 0.0, 0.0, 1.0);
+  Color color = Color(1.0, 0.0, 0.0, 1.0);
+  outColor = vec4(color.r, color.g, color.b, color.a);
   return;
 }
