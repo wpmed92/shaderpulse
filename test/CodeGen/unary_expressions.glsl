c5b638879be18fd37cf81d198f17aa44057b7fd9
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2024-08-29 08:55:26 +0200

    More unary op tests: negate, plus, bitwise/logical not

--- test/CodeGen/unary_expressions.glsl
+++ test/CodeGen/unary_expressions.glsl
@@ -19,6 +19,35 @@ void main() {
     // CHECK-NEXT: spirv.Store "Function" %4, %6 : f32
     float d = ++c;
 
+    // CHECK: %cst1_si32_1 = spirv.Constant 1 : si32
+    // CHECK-NEXT: %9 = spirv.ISub %8, %cst1_si32_1 : si32
+    // CHECK-NEXT: spirv.Store "Function" %0, %9 : si32
     b = --a;
+
+    // CHECK: %cst_f32_2 = spirv.Constant 1.000000e+00 : f32
+    // CHECK-NEXT: %11 = spirv.FSub %10, %cst_f32_2 : f32
+    // CHECK-NEXT: spirv.Store "Function" %4, %11 : f32
     d = --c;
+
+    // CHECK: %12 = spirv.Load "Function" %0 : si32
+    // CHECK-NEXT: %13 = spirv.SNegate %12 : si32
+    int e = -a;
+
+    // CHECK: %15 = spirv.Load "Function" %4 : f32
+    // CHECK-NEXT: %16 = spirv.FNegate %15 : f32
+    c = -c;
+
+    // CHECK: %17 = spirv.Load "Function" %0 : si32
+    // CHECK-NEXT: spirv.Store "Function" %14, %17 : si32
+    e = +a;
+
+    // CHECK: %18 = spirv.Load "Function" %0 : si32
+    // CHECK-NEXT: %19 = spirv.Not %18 : si32
+    e = ~a;
+
+    bool f = true;
+
+    // CHECK: %21 = spirv.Load "Function" %20 : i1
+    // CHECK-NEXT: %22 = spirv.LogicalNot %21 : i1
+    f = !f;
 }
